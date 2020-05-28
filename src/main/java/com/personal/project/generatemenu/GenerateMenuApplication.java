9a0e1a2a365a06d8d98f375aeab1fa85abfa305b
Thaís Paz <thaispaz58@gmail.com>
2020-05-28 20:05:25 -0300

    first commit

---
+++ src/main/java/com/personal/project/generatemenu/GenerateMenuApplication.java
@@ -0,0 +1,13 @@
+package com.personal.project.generatemenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerateMenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/project/generatemenu/GenerateMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.project.generatemenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerateMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
